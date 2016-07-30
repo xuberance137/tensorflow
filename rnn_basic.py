3e8c147c9f18d92653c6f5d0052acdc836633907
Gopal Erinjippurath <gopal.e@gmail.com>
2016-07-30 09:31:46 +0530

    Recurrent Neural Network basic example on text streams

---
+++ rnn_basic.py
@@ -0,0 +1,40 @@
+import nltk
+import csv
+import itertools
+
+vocabulary_size = 8000
+unknown_token = "UNKNOWN_TOKEN"
+sentence_start_token = "SENTENCE_START"
+sentence_end_token = "SENTENCE_END"
+
+# Reading data and appending with sentence Start/Stop tokens
+print "Reading CSV file..."
+with open('data/reddit-comments-2015-08.csv', 'rb') as f:
+    reader = csv.reader(f, skipinitialspace=True)
+    reader.next()
+    # Split full comments into sentences
+    sentences = itertools.chain(*[nltk.sent_tokenize(x[0].decode('utf-8').lower()) for x in reader])
+    # Append SENTENCE_START and SENTENCE_END
+    sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for x in sentences]
+print "Number of parsed sentences : ", len(sentences)
+    
+# Tokenize the sentences into words
+tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
+
+# Count word frequency
+word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
+print "Number of unique word tokens : ", len(word_freq)
+
+vocab = word_freq.most_common(vocabulary_size - 1)
+index_to_word = [x[0] for x in vocab]
+index_to_word.append(unknown_token)
+
+word_to_index = dict([(word, index) for index, word in enumerate(index_to_word)])
+
+print "Vocabulary Size : ", vocabulary_size
+print "Most Frequent word : ", vocab[0][0], vocab[0][1]
+print "Least Frequent word : ", vocab[-1][0], vocab[-1][1]
+
+# for k, v in word_to_index.iteritems():
+# 	print k, v
+
