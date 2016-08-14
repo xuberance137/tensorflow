c278c9459025e83d6405ce4eae265916aa05027b
Gopal Erinjippurath <gopal.e@gmail.com>
2016-08-14 15:26:20 -0700

    Refactoring

--- rnn_basic.py
+++ rnn_basic.py
@@ -177,68 +177,71 @@ class RNNNumpy:
 				num_examples_seen += 1
 
 
-# Reading data and appending with sentence Start/Stop tokens
-print "Reading CSV file..."
-with open('data/reddit-comments-2015-08.csv', 'rb') as f:
-    reader = csv.reader(f, skipinitialspace=True)
-    reader.next()
-    # Split full comments into sentences
-    sentences = itertools.chain(*[nltk.sent_tokenize(x[0].decode('utf-8').lower()) for x in reader])
-    # Append SENTENCE_START and SENTENCE_END
-    sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for x in sentences]
-print "Number of parsed sentences : ", len(sentences)
-    
-# Tokenize the sentences into words
-tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
+### MAIN FUNCTION ###
+if __name__ == '__main__':
+	
+	# Reading data and appending with sentence Start/Stop tokens
+	print "Reading CSV file..."
+	with open('data/reddit-comments-2015-08.csv', 'rb') as f:
+	    reader = csv.reader(f, skipinitialspace=True)
+	    reader.next()
+	    # Split full comments into sentences
+	    sentences = itertools.chain(*[nltk.sent_tokenize(x[0].decode('utf-8').lower()) for x in reader])
+	    # Append SENTENCE_START and SENTENCE_END
+	    sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for x in sentences]
+	print "Number of parsed sentences : ", len(sentences)
+	    
+	# Tokenize the sentences into words
+	tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
 
-# Count word frequency
-word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
-print "Number of unique word tokens : ", len(word_freq)
+	# Count word frequency
+	word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
+	print "Number of unique word tokens : ", len(word_freq)
 
-vocab = word_freq.most_common(vocabulary_size - 1)
-index_to_word = [x[0] for x in vocab]
-index_to_word.append(unknown_token)
+	vocab = word_freq.most_common(vocabulary_size - 1)
+	index_to_word = [x[0] for x in vocab]
+	index_to_word.append(unknown_token)
 
-word_to_index = dict([(word, index) for index, word in enumerate(index_to_word)])
-rev_word_lookup = dict([(index, word) for index, word in enumerate(index_to_word)])
+	word_to_index = dict([(word, index) for index, word in enumerate(index_to_word)])
+	rev_word_lookup = dict([(index, word) for index, word in enumerate(index_to_word)])
 
-print "Vocabulary Size : ", vocabulary_size
-print "Most Frequent word : ", vocab[0][0], vocab[0][1]
-print "Least Frequent word : ", vocab[-1][0], vocab[-1][1]
+	print "Vocabulary Size : ", vocabulary_size
+	print "Most Frequent word : ", vocab[0][0], vocab[0][1]
+	print "Least Frequent word : ", vocab[-1][0], vocab[-1][1]
 
-# Replace words not in dictionary wtih Unknown Token
-for index, sent in enumerate(tokenized_sentences):
-	tokenized_sentences[index] = [w if w in word_to_index else unknown_token for w in sent]
+	# Replace words not in dictionary wtih Unknown Token
+	for index, sent in enumerate(tokenized_sentences):
+		tokenized_sentences[index] = [w if w in word_to_index else unknown_token for w in sent]
 
-print "Example Sentence : ", sentences[0]
-print "Example pre-processed Sentence : ", tokenized_sentences[0]
-print "Example codified Sentence : ", np.asarray([word_to_index[w] for w in tokenized_sentences[0]])
+	print "Example Sentence : ", sentences[0]
+	print "Example pre-processed Sentence : ", tokenized_sentences[0]
+	print "Example codified Sentence : ", np.asarray([word_to_index[w] for w in tokenized_sentences[0]])
 
-# Creating training data
-X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in tokenized_sentences])
-y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized_sentences])
-# for k, v in word_to_index.iteritems():
-# 	print k, v
-print "Training input :", X_train[10]
-print "Training output :", y_train[10]
+	# Creating training data
+	X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in tokenized_sentences])
+	y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized_sentences])
+	# for k, v in word_to_index.iteritems():
+	# 	print k, v
+	print "Training input :", X_train[10]
+	print "Training output :", y_train[10]
 
-np.random.seed(10)
-model = RNNNumpy(vocabulary_size)
-o, s = model.forward_propagation(X_train[10])
-p = model.predict(X_train[10])
-print len(X_train[10])
-print "Model Prediction : ", p 
-print "Model Prediction : ", [rev_word_lookup[index] for index in p]
-# Probablity of prediction is 1/C. random loss = -(1/N)*N*log(1/C) = log(C)
-print "Expected Loss for Random Predictions : ", np.log(vocabulary_size)
-print "Actual Loss from RNN model : ", model.calculate_loss(X_train[:100], y_train[:100])
+	np.random.seed(10)
+	model = RNNNumpy(vocabulary_size)
+	o, s = model.forward_propagation(X_train[10])
+	p = model.predict(X_train[10])
+	print len(X_train[10])
+	print "Model Prediction : ", p 
+	print "Model Prediction : ", [rev_word_lookup[index] for index in p]
+	# Probablity of prediction is 1/C. random loss = -(1/N)*N*log(1/C) = log(C)
+	print "Expected Loss for Random Predictions : ", np.log(vocabulary_size)
+	print "Actual Loss from RNN model : ", model.calculate_loss(X_train[:100], y_train[:100])
 
-#gradient check on simplified model
-grad_check_vocab_size = 100
-np.random.seed(10)
-model_test = RNNNumpy(grad_check_vocab_size, 10, bptt_truncate=1000)
-model_test.gradient_check([0,1,2,3], [1,2,3,4])
+	#gradient check on simplified model
+	grad_check_vocab_size = 100
+	np.random.seed(10)
+	model_test = RNNNumpy(grad_check_vocab_size, 10, bptt_truncate=1000)
+	model_test.gradient_check([0,1,2,3], [1,2,3,4])
 
-model.sgd_step(X_train[10], y_train[10], 0.005)
+	model.sgd_step(X_train[10], y_train[10], 0.005)
 
 
