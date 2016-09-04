9e2b21fc1aa86e0f64308d472726ad3143b19325
Gopal Erinjippurath <gopal.e@gmail.com>
2016-09-04 09:34:34 -0700

    Added Stochastic Gradient descent and partial fit for online processing

--- np_perceptron.py
+++ np_perceptron.py
@@ -8,8 +8,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+from numpy.random import seed
 
-PLOT_DATA = False
+PLOT_DATA = True
 PLOT_CLASSIFIER_OUTPUT = True
 
 def plot_decision_regions(X, y, classifier, resolution=0.02):
@@ -74,6 +75,125 @@ class Perceptron(object):
 
 		return self
 
+class AdalineGD(object):
+	"""
+	parameters: 
+	eta: float, learning rate between 0 and 1
+	n_iter :int, iterate over training set
+	attributes:
+	w_ : 1D arrray of weights affter fitting
+	errors_ : number of misclassifications in each epoch
+	"""
+
+	def __init__(self, eta = 0.01, n_iter = 10):
+		self.eta = eta
+		self.n_iter = n_iter
+
+	def net_input(self, X):
+		return np.dot(X, self.w_[1:]) + self.w_[0]
+
+	def activation(self, X):
+		return self.net_input(X)
+
+	def predict(self, X):
+		return np.where(self.activation(X) >= 0.0, 1, -1)
+
+	def fit(self, X, y):
+		"""
+		X : array like -> shape [n_samples, n_features]
+		y : array like -> shape [n_samples]
+		"""
+		self.w_ = np.zeros(1 + X.shape[1])
+		self.cost_ = []
+
+		for i in range(self.n_iter):
+			output = self.net_input(X)
+			errors = y - output
+			self.w_[1:] += self.eta * X.T.dot(errors) #sum over all samples for each feature
+			self.w_[0] += self.eta * errors.sum()
+			cost = np.sum(errors**2)/2.0
+			self.cost_.append(cost) #keeping track of cost at each epoch
+
+		return self
+
+class AdalineSGD(object):
+	"""
+	parameters: 
+	eta: float, learning rate between 0 and 1
+	n_iter :int, iterate over training set
+	attributes:
+	w_ : 1D arrray of weights affter fitting
+	errors_ : number of misclassifications in each epoch
+	shuffle : bool, shuffle trainign data everu epoch
+	random_state : int, set rnadom state for shuffling and init weights
+	"""
+
+	def __init__(self, eta = 0.01, n_iter = 10, shuffle=True, random_state=None):
+		self.eta = eta
+		self.n_iter = n_iter
+		self.w_initialized = False
+		self.shuffle = shuffle
+		if random_state:
+			seed(random_state)
+
+	def net_input(self, X):
+		return np.dot(X, self.w_[1:]) + self.w_[0]
+
+	def activation(self, X):
+		return self.net_input(X)
+
+	def predict(self, X):
+		return np.where(self.activation(X) >= 0.0, 1, -1)
+
+	def _intialize_weights(self, m):
+		self.w_ = np.zeros(1 + m)
+		self.w_initialized = True
+
+	#updating on a sample basis
+	def _update_weights(self,  xi, target):
+		output = self.net_input(xi)
+		error = (target - output)
+		self.w_[1:] += self.eta * xi.dot(error)
+		self.w_[0] += self.eta * error
+		cost = 0.5 * (error**2)
+		return cost
+
+	def fit(self, X, y):
+		"""
+		X : array like -> shape [n_samples, n_features]
+		y : array like -> shape [n_samples]
+		"""
+		self.w_ = np.zeros(1 + X.shape[1])
+		self.cost_ = []
+
+		for i in range(self.n_iter):
+			if self.shuffle:
+				X, y = self._shuffle(X, y)
+			cost = []
+			for xi, target in zip(X, y):
+				cost.append(self._update_weights(xi, target))
+			average_cost = sum(cost)/len(y)
+			self.cost_.append(average_cost)
+
+		return self
+
+	def partial_fit(self, X, y):
+		if not self.w_initialized:
+			self._intialize_weights(X.shape[1])
+		if y.ravel().shape[0] > 1:
+			for xi, target in zip(X, y):
+				self._update_weights(xi, target)
+		else:
+			self._update_weights(X, y)
+
+		return self
+
+	def _shuffle(self, X, y):
+		#shuffle training data
+		R = np.random.permutation(len(y))
+		return X[R], y[R]
+
+
 
 ### MAIN FUNCTION ###
 if __name__ == '__main__':
@@ -89,17 +209,37 @@ if __name__ == '__main__':
 	ppn = Perceptron(eta=0.1, n_iter=30)
 	ppn.fit(X, y)
 
+	ada1 = AdalineGD(eta=0.01, n_iter=100)
+	ada1.fit(X, y)
+
+	ada2 = AdalineGD(eta=0.001, n_iter=1000)
+	ada2.fit(X, y)
+
+	Xstd = np.copy(X)
+	Xstd[:, 0] = (Xstd[:, 0] - Xstd[:, 0].mean())/ Xstd[:, 0].std()
+	Xstd[:, 1] = (Xstd[:, 1] - Xstd[:, 1].mean())/ Xstd[:, 1].std()	
+
+	ada3 = AdalineGD(eta=0.1, n_iter=1000)
+	ada3.fit(Xstd, y)
+
+	ada4 = AdalineSGD(eta=0.01, n_iter=15, random_state=1)
+	ada4.fit(Xstd, y)
+
 	if PLOT_DATA:
-		plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
-		plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
-		plt.xlabel('sepal length')
-		plt.ylabel('petal length')
-		plt.legend(loc = 'upper left')
+		# plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
+		# plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
+		# plt.xlabel('sepal length')
+		# plt.ylabel('petal length')
+		# plt.legend(loc = 'upper left')
 		
-		plt.figure()
-		plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker = 'o')
-		plt.xlabel('Epochs')
-		plt.ylabel('Misclassification Count')
+		# plt.figure()
+		# plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker = 'o')
+		# plt.xlabel('Epochs')
+		# plt.ylabel('Misclassification Count')
+		fig, ax = plt.subplots(nrows=1, ncols=3, figsize = (8, 6))
+		ax[0].plot(range(1, len(ada1.cost_) +1), np.log10(ada1.cost_), marker='o')
+		ax[1].plot(range(1, len(ada2.cost_) +1), np.log10(ada2.cost_), marker='o')
+		ax[2].plot(range(1, len(ada4.cost_) +1), ada4.cost_, marker='o')
 
 	if PLOT_CLASSIFIER_OUTPUT:
 		plt.figure()
