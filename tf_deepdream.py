2f5eba33d67f6016ac4412d9553e70adf6b8b4d0
Gopal Erinjippurath <gopal.e@gmail.com>
2017-01-21 14:18:40 -0800

    Added command line parameters, channel number and input file name

--- tf_deepdream.py
+++ tf_deepdream.py
@@ -11,6 +11,7 @@ Added DeepDream Calls  03OCT2016
 """
 
 import os
+import sys
 from io import BytesIO
 import numpy as np
 from functools import partial
@@ -78,12 +79,12 @@ def showarray(a, fmt='jpeg'):
 	im.show()
 
 #assumes an image input in range [0,1)
-def savearray(a, filename, fmt='bmp'):
+def savearray(a, filename):
+	fmt  = filename[-3:].upper()
 	a = np.uint8(np.clip(a, 0, 1)*255)
 	im = PIL.Image.fromarray(a, 'RGB')
-	print 'Saved Image file as bmp ...'
-	im.save(filename, fmt)
-
+	print 'Saved Image file as ' + fmt + ' ...'
+	im.save(filename)
 
 def visstd(a, s=0.1):
 	'''Normalize the image range for visualization'''
@@ -223,34 +224,6 @@ def render_lapnorm(t_obj, img0, visfunc=visstd, iter_n=10, step=1.0, octave_n=3,
 			showarray(visfunc(img))
 			print img.shape 
 
-def render_deepdream(t_obj, img0, iter_n=10, step=1.5, octave_n=4, octave_scale=1.4):
-	t_score = tf.reduce_mean(t_obj) # defining the optimization objective
-	t_grad = tf.gradients(t_score, t_input)[0] # behold the power of automatic differentiation!
-
-	# split the image into a number of octaves
-	img = img0
-	octaves = []
-	for i in range(octave_n-1):
-		hw = img.shape[:2]
-		lo = resize(img, np.int32(np.float32(hw)/octave_scale))
-		hi = img-resize(lo, hw)
-		img = lo
-		octaves.append(hi)
-
-	# generate details octave by octave
-	for octave in range(octave_n):
-		if octave>0:
-			hi = octaves[-octave]
-			img = resize(img, hi.shape[:2])+hi
-		for i in range(iter_n):
-			print '. ', octave, i
-			g = calc_grad_tiled(img, t_grad)
-			img += g*(step / (np.abs(g).mean()+1e-7))
-		clear_output()
-		if octave == octave_n-1:
-			showarray(img/255.0)
-			savearray(img/255.0, './data/inception_66.bmp')
-
 def render_deepdreams(t_tensor, img0, iter_n=10, step=1.5, octave_n=4, octave_scale=1.4):
 
 	num_filters = T(layer).get_shape()[-1]
@@ -283,12 +256,50 @@ def render_deepdreams(t_tensor, img0, iter_n=10, step=1.5, octave_n=4, octave_sc
 			if octave == octave_n-1:
 				#showarray(img/255.0)
 				savearray(img/255.0, './data/dreams/inception_' + str(filter_index) + '.bmp')
+				print 'Created dream ' + filter_index + ' ...'
+
+
+def render_deepdream(t_obj_layer, channel, img0, inputfile, iter_n=10, step=1.5, octave_n=4, octave_scale=1.4):
+	
+	t_obj = t_obj_layer[:,:,:,channel]
+	t_score = tf.reduce_mean(t_obj) # defining the optimization objective
+	t_grad = tf.gradients(t_score, t_input)[0] # behold the power of automatic differentiation!
+
+	outputfile = inputfile[:-4] + '_dream_' + str(channel) + '.jpg'
+
+	# split the image into a number of octaves
+	img = img0
+	octaves = []
+	for i in range(octave_n-1):
+		hw = img.shape[:2]
+		lo = resize(img, np.int32(np.float32(hw)/octave_scale))
+		hi = img-resize(lo, hw)
+		img = lo
+		octaves.append(hi)
+
+	# generate details octave by octave
+	for octave in range(octave_n):
+		if octave>0:
+			hi = octaves[-octave]
+			img = resize(img, hi.shape[:2])+hi
+		for i in range(iter_n):
+			print ' . ', octave, i
+			g = calc_grad_tiled(img, t_grad)
+			img += g*(step / (np.abs(g).mean()+1e-7))
+		clear_output()
+		if octave == octave_n-1:
+			showarray(img/255.0)
+			savearray(img/255.0, outputfile)
+			print 'Created dream and storing image ...'
+
 
 ### MAIN FUNCTION ###
 if __name__ == '__main__':
 
 	print "Loading Model..."
 	model_fn = 'models/inception/tensorflow_inception_graph.pb'
+	channel = sys.argv[1] # picking some feature channel to visualize	
+	input_img_filename  = str(sys.argv[2])
 
 	graph = tf.Graph()
 	#sess = tf.Session(graph=graph)
@@ -306,41 +317,34 @@ if __name__ == '__main__':
 	layers = [op.name for op in graph.get_operations() if op.type=='Conv2D' and 'import/' in op.name]
 	feature_nums = [int(graph.get_tensor_by_name(name+':0').get_shape()[-1]) for name in layers]
 
+
 	# Visualizing the network graph. Be sure expand the "mixed" nodes to see their 
-	# internal structure. We are going to visualize "Conv2D" nodes.
+	# internal structure. We are going to visualize "Conv2D" nodes. Only works in ipython notebook
 	# tmp_def = rename_nodes(graph_def, lambda s:"/".join(s.split('_',1)))
 	# show_graph(tmp_def)
 
 	# Picking some internal layer. Note that we use outputs before applying the ReLU nonlinearity
 	# to have non-zero gradients for features with negative initial activations.
 	layer = 'mixed4d_3x3_bottleneck_pre_relu'
-	channel = 65 # picking some feature channel to visualize
+
 
 	# start with a gray image with a little noise
-	img_noise = np.random.uniform(size=(224,224,3)) + 200.0
+	img_noise = np.random.uniform(size=(224,224,3)) + 100.0
 
 	resize = tffunc(np.float32, np.int32)(resize)
 	# Pull in sample image to dream on
-	img0 = PIL.Image.open('./data/texture_bubbles_1400x425.jpg')
-	img0 = np.float32(img0) 
-	#showarray(img0/255.0)
+	img0 = PIL.Image.open(input_img_filename)
+	img0 = np.float32(img0) #making the input image grey so that features shine through
 
-	# render_deepdream(T(layer)[:,:,:,63], img0)	
-	# render_deepdream(T(layer)[:,:,:,64], img0)	
-	# render_deepdream(T(layer)[:,:,:,65], img0)	
-	# render_deepdream(T(layer)[:,:,:,66], img0, iter_n=2)	
-	# render_deepdream(T(layer)[:,:,:,66], img0)	
-	img0 = np.float32(img0) #makign the input image grey so that features shine through
-	render_deepdream(T(layer)[:,:,:,66], img0, iter_n=20, octave_n=3)	
-	#render_deepdreams(T(layer), img0, iter_n=20, octave_n=5)	
+	render_deepdream(T(layer), channel, img0, input_img_filename, iter_n=5, octave_n=2)	
 
 	if DEBUG_PRINT:	
-		# for name in layers:
-		# 	print graph.get_tensor_by_name(name+':0') 
+		for name in layers:
+			print graph.get_tensor_by_name(name+':0') 
 		print "Number of Layers : ", len(layers)
 		print "Total number of feature channels : ", sum(feature_nums)
 		print feature_nums
-		print T(layer).get_shape()
+		print T(layer)[:,:,:,channel].get_shape()
 
 	if TEST_LAPLACIAN:
 		render_naive(T(layer)[:,:,:,channel], img_noise)
